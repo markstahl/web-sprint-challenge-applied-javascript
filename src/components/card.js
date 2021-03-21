93597b06fe07761a5a24ea745c14757cb91bf71e
Mark Stahl <markstahl0219@gmail.com>
2021-03-21 14:44:46 -0400

    mvp

--- src/components/card.js
+++ src/components/card.js
@@ -32,14 +32,7 @@ const Card = (article) => {
 
 import axios from 'axios';
 const cardAppender = (selector) => {
-  // TASK 6
-  // ---------------------
-  // Implement this function that takes a css selector as its only argument.
-  // It should obtain articles from this endpoint: `https://lambda-times-api.herokuapp.com/articles`
-  // However, the articles do not come organized in a single, neat array. Inspect the response closely!
-  // Create a card from each and every article object in the response, using the Card component.
-  // Append each card to the element in the DOM that matches the selector passed to the function.
-  //
+
   axios.get("https://lambda-times-api.herokuapp.com/articles")
     .then(res => {
       console.log(res.data.articles);
