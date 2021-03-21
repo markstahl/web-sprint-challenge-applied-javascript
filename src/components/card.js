fec3548d784564de60be2a61b187205cef863dae
Mark Stahl <markstahl0219@gmail.com>
2021-03-21 14:43:00 -0400

    mvp

--- src/components/card.js
+++ src/components/card.js
@@ -1,24 +1,36 @@
 const Card = (article) => {
-  // TASK 5
-  // ---------------------
-  // Implement this function, which should return the markup you see below.
-  // It takes as its only argument an "article" object with `headline`, `authorPhoto` and `authorName` properties.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
-  //
-  // <div class="card">
-  //   <div class="headline">{ headline }</div>
-  //   <div class="author">
-  //     <div class="img-container">
-  //       <img src={ authorPhoto }>
-  //     </div>
-  //     <span>By { authorName }</span>
-  //   </div>
-  // </div>
-  //
+
+  const cardDiv = document.createElement('div');
+  const headlineDiv = document.createElement('div');
+  const authorDiv = document.createElement('div');
+  const imgContainDiv = document.createElement('div');
+  const authorImg = document.createElement('img');
+  const authorName = document.createElement('span');
+
+  cardDiv.classList.add('card');
+  headlineDiv.classList.add('headline');
+  authorDiv.classList.add('author');
+  imgContainDiv.classList.add('img-container');
+
+  headlineDiv.textContent = article.headline;
+  authorImg.src = article.authorPhoto;
+  authorName.textContent = `By ${article.authorName}`;
+
+  cardDiv.addEventListener('click', function (event) {
+    console.log(headlineDiv.textContent);
+  })
+
+  cardDiv.appendChild(headlineDiv);
+  cardDiv.appendChild(authorDiv);
+  authorDiv.appendChild(imgContainDiv);
+  imgContainDiv.appendChild(authorImg);
+  authorDiv.appendChild(authorName);
+
+  return cardDiv;
 }
 
+
+import axios from 'axios';
 const cardAppender = (selector) => {
   // TASK 6
   // ---------------------
@@ -28,6 +40,20 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get("https://lambda-times-api.herokuapp.com/articles")
+    .then(res => {
+      console.log(res.data.articles);
+      console.log(res.data.articles.javascript)
+      Object.keys(res.data.articles).forEach(topic => {
+        
+        res.data.articles[topic].forEach(entry => {
+          document.querySelector(selector).appendChild(Card(entry));
+        })
+      })
+    })
+    .catch(err => {
+      console.log('Error');
+    })
 }
 
 export { Card, cardAppender }
--- src/components/header.js
+++ src/components/header.js
@@ -1,25 +1,29 @@
 const Header = (title, date, temp) => {
-  // TASK 1
-  // ---------------------
-  // Implement this function taking `title`, `date` and `temp` as its 3 args and returning the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup exactly!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  //  <div class="header">
-  //    <span class="date">{ date }</span>
-  //    <h1>{ title }</h1>
-  //    <span class="temp">{ temp }</span>
-  //  </div>
-  //
+
+  const headerDiv = document.createElement('div');
+  const dateSpan = document.createElement('span');
+  const titleHeader = document.createElement('h1');
+  const tempSpan = document.createElement('span');
+
+  dateSpan.textContent = date;
+  titleHeader.textContent = title;
+  tempSpan.textContent = temp;
+
+  headerDiv.classList.add('header');
+  dateSpan.classList.add('date');
+  tempSpan.classList.add('temp');
+
+  headerDiv.appendChild(dateSpan);
+  headerDiv.appendChild(titleHeader);
+  headerDiv.appendChild(tempSpan);
+
+  return headerDiv;
 }
 
 const headerAppender = (selector) => {
-  // TASK 2
-  // ---------------------
-  // Implement this function taking a css selector as its only argument.
-  // It should create a header using the Header component above, passing arguments of your choosing.
-  // It should append the header to the element in the DOM that matches the given selector.
-  //
+
+  const newHeader = Header('Lambda Times', 'March 21, 2021', '60°');
+  document.querySelector(selector).appendChild(newHeader);
 }
 
 export { Header, headerAppender }
